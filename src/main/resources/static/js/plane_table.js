cf35d04978681437b14df7d637c611b5dd9b03ed
Carolien Gilbers <carolien.gilbers@capgemini.com>
2017-12-06 12:47:23 +0100

    added tank button which sets the current fuel level to maxfuel

--- src/main/resources/static/js/plane_table.js
+++ src/main/resources/static/js/plane_table.js
@@ -4,10 +4,29 @@ $(document).ready(function(){
         columns: [
             {"data" : "serialNumber"},
             {"data" : "maxFuel" },
-            {"data" : "currentFuel"}
+            {"data" : "currentFuel"},
+            {"defaultContent" : "<button class='btn btn-default'>Tank</button>"}
         ]
     });
 
     getPlanes();
+
+    var table = $('#plane-table').DataTable();
+
+    $('#plane-table tbody').on( 'click', 'button', function () {
+        var data = table.row( $(this).parents('tr') ).data();
+
+        console.log(data['id']);
+
+        $.ajax({
+            url:"http://localhost:8080/api/plane/tank/" + data['id'],
+            type:"put",
+            success: function(result){
+                console.log("tanked!");
+                getPlanes();
+            }
+        });
+    } );
 });
+
 
